4fc6b8461445eaf82441cbc8395e1de9f779676a
Ucatbin <1413709774@qq.com>
2025-06-03 22:44:49 +0800

    Add two items and an item group

--- src/main/java/unsafeloots/UnsafeLoots.java
+++ src/main/java/unsafeloots/UnsafeLoots.java
@@ -4,6 +4,8 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import unsafeloots.item.ModItemGroups;
+import unsafeloots.item.ModItems;
 
 public class UnsafeLoots implements ModInitializer {
 	public static final String MOD_ID = "unsafe-loots";
@@ -11,6 +13,7 @@ public class UnsafeLoots implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/unsafeloots/UnsafeLootsClient.java
+++ src/main/java/unsafeloots/UnsafeLootsClient.java
@@ -2,7 +2,7 @@ package unsafeloots;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class UnsafeLootsClinet implements ClientModInitializer {
+public class UnsafeLootsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
